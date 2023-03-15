59ead6f03b1beb29d60ee27dc1954d3676ba82f2
sheila.orejuela <she.or.va@gmail.com>
2023-03-15 16:42:11 +0100

    test producto

--- Junit5/src/testing/testProducto.java
+++ Junit5/src/testing/testProducto.java
@@ -14,24 +14,37 @@ class testProducto {
 	
 	/**
 	 *  Test 1 : Pvc: 10 * Beneficio: 1,30
-	 *  Resultado esperado: PVC*BENEFICIO = 13
+	 *  Resultado esperado: PVC*BENEFICIO = 13, para ello llamamos al metodo
 	 */
 	@Test
 	void damePvpSinIVACorrecto() {
 		double resultadoEsperado, resultado;
-		resultadoEsperado = 13;
 		prod.setPvc(10);
 		prod.setBeneficio(1.30);
 		resultado = prod.getPvc()*prod.getBeneficio();
+		//   IMPORTANTE !!
+		resultadoEsperado = prod.damePvp();
 		
 		assertEquals(resultadoEsperado, resultado);
 		
 	}
 	
-	
+	/**
+	 * Test 1: Se le indica un pvp de 13, y el iva es :1.21
+	 * El resultado esperado es 15.73
+	 */
 	@Test
 	void damePvpConIVACorrecto() {
-		double resultadoEsperado, resultado;
+		double pvp,resultadoEsperado, resultado;
+		prod.setPvc(10);
+		prod.setBeneficio(1.30);
+		pvp = prod.getPvc()*prod.getBeneficio();
+		
+		prod.setIva(1.21);
+		resultado = pvp*prod.getIva();
+		resultadoEsperado = prod.damePvpIva();
+		
+		assertEquals(resultado, resultadoEsperado);
 	}
 	
 	
